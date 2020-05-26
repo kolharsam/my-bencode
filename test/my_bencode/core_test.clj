3f7e08337428c5df08862c9f597c7368be1bea1f
Sameer Kolhar <kolhar730@gmail.com>
2020-05-26 13:39:00 +0530

    Add test for helpers
    the one helper: gen-byte-seq

--- test/my_bencode/core_test.clj
+++ test/my_bencode/core_test.clj
@@ -1,7 +1,18 @@
 (ns my-bencode.core-test
-  (:require [clojure.test :refer :all]
-            [my-bencode.core :refer :all]))
+  (:require [clojure.test :refer [deftest is are testing]]
+            [my-bencode.core :as my-ben :refer [gen-byte-seq read-netstring write-netstring]]))
 
-(deftest a-test
-  (testing "Trial Test"
-    (is (not (= 0 1)))))
+(def ^{:private true} hello "Hello, world!")
+(def ^{:private true} ham "Ham, Cheese & Bread")
+
+;; gen-byte-seq tests
+
+(deftest helper-tests
+  (testing "gen-byte-seq method"
+    (is (.getBytes hello "UTF-8") (my-ben/gen-byte-seq hello))
+    (is (.getBytes ham "UTF-8") (my-ben/gen-byte-seq ham))))
+
+;; read-netstring tests
+
+
+;; write-netstring tests
