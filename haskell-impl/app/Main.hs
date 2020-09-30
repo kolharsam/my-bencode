8dcd1ebc36099c64a83afb8b1533b01b1bf50103
Sameer Kolhar <kolhar730@gmail.com>
2020-09-30 22:53:36 +0530

    add new test case

--- haskell-impl/app/Main.hs
+++ haskell-impl/app/Main.hs
@@ -9,3 +9,4 @@ main = do
   print $ readInput "[1,2,3,4,5]"
   print $ readInput "[1,2,-4,-5,sdfsdf,sddfdf]"
   print $ readInput "[1,2,-4,-5,abc,asdag, [1,2,3], [asda,gaga]]"
+  print $ readInput "[\"nancy\", \"germany\", 56, 53, -3, [], []]"
